bf76a48a3a4433b3400ef0b1284c769937344a04
saleh alghusson <salehg@live.com>
2017-08-09 00:01:12 -0400

    fixes

--- flask/static-website/__init__.py
+++ flask/static-website/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def homepage( ):
- return "Hola!"
+@app.route("/")
+def homepage():
+    return "Hola!"
 if __name__ == "__main__":
- app.run()
+    app.run()
