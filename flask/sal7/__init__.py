262f347005149c72ee013bf222a973afe87838a5
saleh <sal7@qirh.local>
2017-08-13 01:09:46 -0400

    small stuff

--- flask/sal7/__init__.py
+++ flask/sal7/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return "Hola!"
-if __name__ == "__main__":
-    app.run()
+
+@app.route ( '/index' )
+def index ():
+	return render_template('index.html',
+						    title='mi salon')
