73852d8ebf412c2f08bc9194d3e333787d007ebd
saleh <sal7@qirh.local>
2017-09-10 20:58:21 -0500

    added projects

--- flask/sala/__init__.py
+++ flask/sala/__init__.py
@@ -6,8 +6,31 @@ app = Flask(__name__)
 def index ():
 	return render_template('index.html',
 						    title='saleh')
-
 @app.route ("/cv")
 def cv ():
 	return render_template('cv.html',
+						    title='cv')
+@app.route ("/projects")
+def cv ():
+	return render_template('projects.html',
+						    title='cv')
+@app.route ("/artnavigator")
+def cv ():
+	return render_template('artnavigator.html',
+						    title='cv')
+@app.route ("/myqueue")
+def cv ():
+	return render_template('myqueue.html',
+						    title='cv')
+@app.route ("/apex")
+def cv ():
+	return render_template('apex.html',
+						    title='cv')
+@app.route ("/stockexchange")
+def cv ():
+	return render_template('stockexchange.html',
+						    title='cv')
+@app.route ("/emailmanager")
+def cv ():
+	return render_template('emailmanager.html',
 						    title='cv')
