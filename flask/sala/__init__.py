d51d25e1ca8f4c1c0645097ab291e0794a3ac2c7
saleh alghusson <salehg@live.com>
2017-09-17 11:31:41 -0500

    Added graph

--- flask/sala/__init__.py
+++ flask/sala/__init__.py
@@ -10,17 +10,21 @@ def index():
 def cv():
 	return render_template('cv.html',
 						    title='cv')
+@app.route ("/graph")
+def graph():
+	return render_template('graph.html',
+						    title='graph')
 @app.route ("/projects")
 def projects():
 	return render_template('projects.html',
 						    title='projects')
 @app.route ("/artnavigator")
 def artnavigator():
-	return render_template('artnavigator.html',
+	return render_template('proj-artnavigator.html',
 						    title='artnavigator')
 @app.route ("/myqueue")
 def myqueue():
-	return render_template('myqueue.html',
+	return render_template('proj-myqueue.html',
 						    title='myqueue')
 @app.route ("/apex")
 def apex():
