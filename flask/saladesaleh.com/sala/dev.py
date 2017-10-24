0611baf00dbff2d493c8a7353b00e68035dbec3d
saleh alghusson <salehg@live.com>
2017-10-24 12:54:30 -0500

    added small comment for refrence

--- flask/saladesaleh.com/sala/dev.py
+++ flask/saladesaleh.com/sala/dev.py
@@ -1,4 +1,6 @@
 from __init__ import app
 
+#ssl tutorial from here: https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
+
 if __name__ == "__main__" :
 	app.run(host='0.0.0.0', use_reloader=True, debug=True)
