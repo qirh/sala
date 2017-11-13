c94667bb6caa618176ea19b8377d33c4b1152d40
saleh alghusson <salehg@live.com>
2017-11-13 11:23:19 -0600

    working on the projects page, it now works, but needs polishing

--- flask/saladesaleh.com/sala/__init__.py
+++ flask/saladesaleh.com/sala/__init__.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/en")
 @app.route("/en/")
+@app.route("/en/index")
 @app.route ("/index")
 def index():
 	return render_template('index.html', title='Saleh')
