76fc1656c2081789bce79ba3afbede8b6b0a6b01
saleh alghusson <salehg@live.com>
2017-12-02 17:48:50 -0600

    trying to fix coding

--- flask/saladesaleh.com/sala/__init__.py
+++ flask/saladesaleh.com/sala/__init__.py
@@ -49,62 +49,62 @@ def texas():
 @app.route("/es/")
 @app.route ("/es/index")
 def es_index():
-	return render_template('/es/index.html', title='Saléh')
+	return render_template('/es/index.html', title=u'Saléh')
 @app.route ("/es/cv")
 def es_cv():
-	return render_template('/es/cv.html', title='Saléh')
+	return render_template('/es/cv.html', title=u'Saléh')
 @app.route ("/es/graph")
 def es_graph():
-	return render_template('/es/graph.html', title='Saléh')
+	return render_template('/es/graph.html', title=u'Saléh')
 @app.route ("/es/projects")
 def es_projects():
-	return render_template('/es/projects.html', title='Saléh')
+	return render_template('/es/projects.html', title=u'Saléh')
 @app.route ("/es/cambridgeart")
 def es_cambridgeart():
-	return render_template('/es/proj-cambridgeart.html', title='Saléh')
+	return render_template('/es/proj-cambridgeart.html', title=u'Saléh')
 @app.route ("/es/myqueue")
 def es_myqueue():
-	return render_template('/es/proj-myqueue.html', title='Saléh')
+	return render_template('/es/proj-myqueue.html', title=u'Saléh')
 @app.route ("/es/gameoflife")
 def es_gameoflife():
-	return render_template('/es/proj-gameoflife.html', title='Saléh')
+	return render_template('/es/proj-gameoflife.html', title=u'Saléh')
 @app.route ("/es/downing")
 def es_downing():
-	return render_template('/es/proj-downing.html', title='Saléh')
+	return render_template('/es/proj-downing.html', title=u'Saléh')
 @app.route ("/es/texas")
 def es_texas():
-	return render_template('/es/proj-texas.html', title='Saléh')
+	return render_template('/es/proj-texas.html', title=u'Saléh')
 
 ###	Arabic
 @app.route("/ar")
 @app.route("/ar/")
 @app.route ("/ar/index")
 def ar_index():
-	return render_template('/ar/index.html', title='صالح')
+	return render_template('/ar/index.html', title=u'صالح')
 @app.route ("/ar/cv")
 def ar_cv():
-	return render_template('/ar/cv.html', title='صالح')
+	return render_template('/ar/cv.html', title=u'صالح')
 @app.route ("/ar/graph")
 def ar_graph():
-	return render_template('/ar/graph.html', title='صالح')
+	return render_template('/ar/graph.html', title=u'صالح')
 @app.route ("/ar/projects")
 def ar_projects():
-	return render_template('/ar/projects.html', title='صالح')
+	return render_template('/ar/projects.html', title=u'صالح')
 @app.route ("/ar/cambridgeart")
 def ar_cambridgeart():
-	return render_template('/ar/proj-cambridgeart.html', title='صالح')
+	return render_template('/ar/proj-cambridgeart.html', title=u'صالح')
 @app.route ("/ar/myqueue")
 def ar_myqueue():
-	return render_template('/ar/proj-myqueue.html', title='صالح')
+	return render_template('/ar/proj-myqueue.html', title=u'صالح')
 @app.route ("/ar/gameoflife")
 def ar_gameoflife():
-	return render_template('/ar/proj-gameoflife.html', title='صالح')
+	return render_template('/ar/proj-gameoflife.html', title=u'صالح')
 @app.route ("/ar/downing")
 def ar_downing():
-	return render_template('/ar/proj-downing.html', title='صالح')
+	return render_template('/ar/proj-downing.html', title=u'صالح')
 @app.route ("/ar/texas")
 def ar_texas():
-	return render_template('/ar/proj-texas.html', title='صالح')
+	return render_template('/ar/proj-texas.html', title=u'صالح')
 
 @app.errorhandler(404)
 def own_404_page(error):
