5efaf4e0ede26020eb4d2c34440fd3bcf574ea12
qirh <almto3@hotmail.com>
2018-01-04 18:31:15 +0000

    changed to python 3

--- flask/saladesaleh.com/sala/venv/bin/activate.fish
+++ flask/saladesaleh.com/sala/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/var/www/flask/venv"
+set -gx VIRTUAL_ENV "/var/www/site/flask/saladesaleh.com/sala/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
