e33386f2a0b696cca2145e800a58e6a7c772f4c0
qirh <almto3@hotmail.com>
2018-01-04 18:43:48 +0000

    converted the other site to python3

--- flask/salehjones.com/downing/venv/bin/activate.fish
+++ flask/salehjones.com/downing/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/var/www/FlaskApp/DowningJones/venv"
+set -gx VIRTUAL_ENV "/var/www/site/flask/salehjones.com/downing/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
