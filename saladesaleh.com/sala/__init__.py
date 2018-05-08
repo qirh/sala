40a2fc827890e418212682daa1c32fdaee292a28
saleh <almto3@hotmail.com>
2018-05-08 14:30:08 -0400

    added fonts

--- saladesaleh.com/sala/__init__.py
+++ saladesaleh.com/sala/__init__.py
@@ -60,14 +60,6 @@ def projects():
 @app.route ("/test")
 def test():
     return render_template('test.html', title = 'Saleh')
-@app.route("/en/test2")
-@app.route ("/test2")
-def test2():
-    return render_template('test2.html', title = 'Saleh')
-@app.route("/en/test3")
-@app.route ("/test3")
-def test3():
-    return render_template('test3.html', title = 'Saleh')
 @app.route("/en/cambridgeart")
 @app.route ("/cambridgeart")
 def cambridgeart():
