28f1512cfbfc1169f3ca000abb7b08a63fe8f83d
saleh <almto3@hotmail.com>
2018-06-20 10:27:19 -0400

    merging changes to arabic

--- saleh.alghusson.com/sala/static/js/language.js
+++ saleh.alghusson.com/sala/static/js/language.js
@@ -1,14 +1,27 @@
+var languages = ["en", "es", "pt", "fr", "de", "ar", "he"];
+var lang_default = 'en' //TODO: set default langugae based on broweser prefrence
 
-function checkLanguage(currentLanguage, newLanguage) {
-    var languages = ["en", "es", "pt", "fr", "de", "ar", "he"];
-    if(currentLanguage == newLanguage)
+function check_language(current_lang, new_lang) {
+
+    if(current_lang == new_lang)
         return;
-    else if ($.inArray(newLanguage, languages) > -1)
-        setLanguage(newLanguage);
+    else if ($.inArray(new_lang, languages) > -1)
+        set_language_cookie(new_lang);
     else
-        return;
+        set_language_cookie(lang_default);
 }
-function setLanguage(language) {
-    $.cookie('lang', language, { expires: 7, path:'/' });
+function set_language_cookie(lang) {
+    $.cookie('lang', lang, { expires: 7, path:'/' });
     location.reload();
 }
+function get_language_cookie() {
+    var lang = $.cookie('lang');
+
+    if(lang)
+        return lang;
+    return lang_default;
+}
+function ready_cookie() {
+    $.cookie('lang', get_language_cookie(), { expires: 7, path:'/' });
+}
+document.addEventListener('DOMContentLoaded', function() {ready_cookie();}, false);
