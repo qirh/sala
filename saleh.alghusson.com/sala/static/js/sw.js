7be5a312cc161cc7d4a246d3a8ba8ff34c73cd53
saleh <salehg@live.com>
2018-08-01 16:53:15 -0400

    fixed typo in manifest

--- saleh.alghusson.com/sala/static/js/sw.js
+++ saleh.alghusson.com/sala/static/js/sw.js
@@ -1,30 +1,12 @@
-/*
- *
- *  Air Horner
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-
-const version = "0.8.5";
+const version = "1.0";
 const cacheName = 'saleh-${version}';
 self.addEventListener('install', e => {
   const timeStamp = Date.now();
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        '/templates/en/layout.html?timestamp=${timeStamp}',
+        '/templates/dropdown.html?timestamp=${timeStamp}',
+        '/templates/en/skeleton.html?timestamp=${timeStamp}',
           '/templates/en/index.html?timestamp=${timeStamp}',
           '/static/icons/github.svg',
           '/static/icons/website.svg',
